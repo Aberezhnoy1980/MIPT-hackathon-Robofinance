a29b221095a66387a9cef47a02b31562b0ecaf94
maratgaliulin <egomarat@gmail.com>
2024-12-10 06:48:48 +1100

    подрихтовал методы log_regression_fit и log_regression_predict

--- working_with_datasets/models_raw/classic_models/logistic_regression/logistic_regression_fit.py
+++ working_with_datasets/models_raw/classic_models/logistic_regression/logistic_regression_fit.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from optbinning import BinningProcess
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -6,10 +7,16 @@ import numpy as np
 import pandas as pd
 import pickle
 
-def log_regression_fit(X_train:np.ndarray, y_train:np.ndarray, variable_names:list, model_save_dir:str) -> None:
+def log_regression_fit(final_df:pd.DataFrame, model_save_dir:str) -> list:
 
+    variable_names = [x for x in final_df.columns if x != 'target']
     file_dir = model_save_dir + 'logistic_regression_model.pkl'    
-        
+
+    data = final_df[variable_names]
+    target = final_df['target']
+    
+    X_train, _, y_train, _ = train_test_split(data.values, target.values, random_state=42, test_size=0.3)
+
     binning_process = BinningProcess(
     variable_names, max_n_bins=6, 
     max_n_prebins=6,
@@ -25,13 +32,21 @@ def log_regression_fit(X_train:np.ndarray, y_train:np.ndarray, variable_names:li
 
     log_reg.fit(X_train, y_train)
 
-    print(binning_process.information(print_level=1))
-
     model_summary = binning_process.summary()
 
     cols_to_drop = []
 
+    for name in model_summary['name']:
+        if (
+            (model_summary.loc[model_summary['name'] == name]['iv'].values[0] <= 0.01) 
+            or 
+            (model_summary.loc[model_summary['name'] == name]['gini'].values[0] <= 0.03)
+        ):
+            cols_to_drop.append(name)
+
     with open(file_dir, 'wb') as file:
         pickle.dump(log_reg, file)
+
+    return cols_to_drop
     
     
--- working_with_datasets/models_raw/classic_models/logistic_regression/logistic_regression_predict.py
+++ working_with_datasets/models_raw/classic_models/logistic_regression/logistic_regression_predict.py
@@ -1,16 +1,31 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 import pickle
 
-def log_regression_predict_with_target(test_data:pd.DataFrame, test_target:pd.Series, model_save_dir:str) -> None:
+def log_regression_predict_with_target(final_df:pd.DataFrame, cols_to_drop:list, model_save_dir:str) -> None:
+    
+    if(not cols_to_drop):
+        final_data_df = final_df.drop(columns=cols_to_drop)        
+    else:
+        final_data_df = final_df
+        
+
+    variable_names = [x for x in final_data_df.columns if x != 'target']
+    data = final_data_df[variable_names]
+    target = final_data_df['target']
+
+    _, X_test, _, y_test = train_test_split(data.values, target.values, random_state=42, test_size=0.3)
 
     file_dir = model_save_dir + 'logistic_regression_model.pkl'
     # target = test_df['target']
     with open(file_dir, 'rb') as file:
         log_reg_model = pickle.load(file)
 
-    log_reg_predict = log_reg_model.predict(test_data)
-    print(f'LogRegression test score: {log_reg_model.score(test_data, test_target)}')
+    log_reg_predict = log_reg_model.predict(X_test)
+    print(f'LogRegression test score: {log_reg_model.score(X_test, y_test)}')
+    print(classification_report(y_test, log_reg_predict))
 
     return log_reg_predict
 
